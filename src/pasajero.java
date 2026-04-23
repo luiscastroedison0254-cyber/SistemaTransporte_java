e69c9a3863ac26c4f476f1cf036fca6ca7f25f6c
Luis Angel <luiscastroedison0254@gmail.com>
2026-04-22 19:15:44 -0600

    Update pasajero.java

--- src/pasajero.java
+++ src/pasajero.java
@@ -1,1 +1,11 @@
+package com.mycompany.sistemadetransporte_poo;
 
+public class Pasajero {
+    String nombre;
+    String destino;
+
+    public Pasajero(String nombre, String destino) {
+        this.nombre = nombre;
+        this.destino = destino;
+    }
+}
